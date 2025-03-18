b1c8b125ace8111a5fef9492de685ce9fdf8e9a3
Pedro Lima <pedrolimaa354@gmail.com>
2025-03-18 16:39:53 -0300

    feat: creating a model.

---
+++ src/main/java/com/example/banco_digital/model/Conta.java
@@ -0,0 +1,79 @@
+package com.example.bancodigital.model;
+
+import java.time.LocalDate;
+
+public class Conta {
+    private String numero;
+    private String agencia;
+    private String nomeTitular;
+    private String cpfTitular;
+    private LocalDate dataAbertura;
+    private double saldoInicial;
+    private boolean ativa;
+    private String tipo; // "corrente", "poupanca", "salario"
+
+    // Getters and Setters
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(String agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getNomeTitular() {
+        return nomeTitular;
+    }
+
+    public void setNomeTitular(String nomeTitular) {
+        this.nomeTitular = nomeTitular;
+    }
+
+    public String getCpfTitular() {
+        return cpfTitular;
+    }
+
+    public void setCpfTitular(String cpfTitular) {
+        this.cpfTitular = cpfTitular;
+    }
+
+    public LocalDate getDataAbertura() {
+        return dataAbertura;
+    }
+
+    public void setDataAbertura(LocalDate dataAbertura) {
+        this.dataAbertura = dataAbertura;
+    }
+
+    public double getSaldoInicial() {
+        return saldoInicial;
+    }
+
+    public void setSaldoInicial(double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+
+    public boolean isAtiva() {
+        return ativa;
+    }
+
+    public void setAtiva(boolean ativa) {
+        this.ativa = ativa;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
